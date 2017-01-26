499d1b6e291072c0d715f86285f7463b04434a09
Tim Foerster <github@mailserver.1n3t.de>
2017-01-26 23:01:02 +0000

    Log metadata
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- controllers/mirror_service.go
+++ controllers/mirror_service.go
@@ -1,15 +1,20 @@
 package controllers
 
 import (
+	log "github.com/Sirupsen/logrus"
 	pb "github.com/mirrorhub-io/platform/controllers/proto"
 	"github.com/mirrorhub-io/platform/models"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
 )
 
 type mirrorServiceServer struct {
 }
 
 func (m *mirrorServiceServer) Get(ctx context.Context, request *pb.MirrorGetRequest) (*pb.MirrorGetResponse, error) {
+	md, _ := metadata.FromContext(ctx)
+	log.Info("Metadata: ")
+	log.Info(md)
 	return &pb.MirrorGetResponse{
 		Mirrors: models.MirrorList(10, 0).ToProto(),
 	}, nil
