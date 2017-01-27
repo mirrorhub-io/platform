6d313a09f4333c2e65e402d6ee42194f2c412bb0
Tim Foerster <github@mailserver.1n3t.de>
2017-01-27 19:14:09 +0000

    Update proto files
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- controllers/proto/api.pb.gw.go
+++ controllers/proto/api.pb.gw.go
@@ -57,6 +57,33 @@ func request_MirrorService_Get_0(ctx context.Context, marshaler runtime.Marshale
 
 }
 
+func request_MirrorService_Find_0(ctx context.Context, marshaler runtime.Marshaler, client MirrorServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq MirrorFindRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["client_token"]
+	if !ok {
+		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "client_token")
+	}
+
+	protoReq.ClientToken, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, err
+	}
+
+	msg, err := client.Find(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_MirrorService_Create_0(ctx context.Context, marshaler runtime.Marshaler, client MirrorServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq Mirror
 	var metadata runtime.ServerMetadata
@@ -227,6 +254,34 @@ func RegisterMirrorServiceHandler(ctx context.Context, mux *runtime.ServeMux, co
 
 	})
 
+	mux.Handle("GET", pattern_MirrorService_Find_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, req)
+		if err != nil {
+			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
+		}
+		resp, md, err := request_MirrorService_Find_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_MirrorService_Find_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_MirrorService_Create_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(ctx)
 		defer cancel()
@@ -261,11 +316,15 @@ func RegisterMirrorServiceHandler(ctx context.Context, mux *runtime.ServeMux, co
 var (
 	pattern_MirrorService_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "mirrors"}, ""))
 
+	pattern_MirrorService_Find_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "mirrors", "client_token"}, ""))
+
 	pattern_MirrorService_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "mirrors"}, ""))
 )
 
 var (
 	forward_MirrorService_Get_0 = runtime.ForwardResponseMessage
+
+	forward_MirrorService_Find_0 = runtime.ForwardResponseMessage
 
 	forward_MirrorService_Create_0 = runtime.ForwardResponseMessage
 )
