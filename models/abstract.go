463dd54947d4bded3a5e5fe050237a932e993834
Tim Foerster <github@mailserver.1n3t.de>
2017-01-29 22:30:01 +0000

    Provide ability to control database path over environment
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- models/abstract.go
+++ models/abstract.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"os"
 )
 
 var conn *gorm.DB
@@ -15,7 +16,11 @@ func Connection() *gorm.DB {
 }
 
 func db_init() {
-	db, _ := gorm.Open("sqlite3", "/tmp/gorm.db")
+	path := "/tmp/mirrorhub.db"
+	if os.Getenv("DB_PATH") != "" {
+		path = os.Getenv("DB_PATH")
+	}
+	db, _ := gorm.Open("sqlite3", path)
 	db.AutoMigrate(&Mirror{})
 	db.AutoMigrate(&Service{})
 	db.AutoMigrate(&Contact{})
